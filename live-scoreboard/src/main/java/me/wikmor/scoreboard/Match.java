729f20763de6296b60c6c79bdf07a885afcd8011
wikmor <wikmor@proton.me>
2024-05-08 13:15:08 +0200

    Create a class blueprint for a match

---
+++ live-scoreboard/src/main/java/me/wikmor/scoreboard/Match.java
@@ -0,0 +1,24 @@
+package me.wikmor.scoreboard;
+
+public class Match {
+
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeScore;
+    private int awayScore;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+}
