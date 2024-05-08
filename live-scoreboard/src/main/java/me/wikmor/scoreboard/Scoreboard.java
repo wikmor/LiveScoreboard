df4f523353b78f616ae57a2b4f7d719ba3520f0f
wikmor <wikmor@proton.me>
2024-05-08 13:21:08 +0200

    Add method for adding a new Match to the Scoreboard

--- live-scoreboard/src/main/java/me/wikmor/scoreboard/Scoreboard.java
+++ live-scoreboard/src/main/java/me/wikmor/scoreboard/Scoreboard.java
@@ -1,5 +1,18 @@
 package me.wikmor.scoreboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoreboard {
+
+    private final List<Match> matches = new ArrayList<>();
+
+    public void addMatch(String home, String away) {
+        matches.add(new Match(home, away));
+    }
+
+    public List<Match> getMatches() {
+        return new ArrayList<>(matches);
+    }
 
 }
--- live-scoreboard/src/test/java/me/wikmor/scoreboard/ScoreboardTest.java
+++ live-scoreboard/src/test/java/me/wikmor/scoreboard/ScoreboardTest.java
@@ -1,5 +1,27 @@
 package me.wikmor.scoreboard;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class ScoreboardTest {
+
+    @Test
+    void addMatch_shouldReturnNotEmptyList_whenMatchAddedToScoreboard() {
+        // Given
+        Scoreboard scoreboard = new Scoreboard();
+        String homeTeam = "Mexico";
+        String awayTeam = "Canada";
+        Match expectedMatch = new Match(homeTeam, awayTeam);
+
+        // When
+        scoreboard.addMatch(homeTeam, awayTeam);
+
+        // Then
+        assertFalse(scoreboard.getMatches().isEmpty());
+        assertEquals(expectedMatch.getHomeTeam(), scoreboard.getMatches().get(0).getHomeTeam());
+        assertEquals(expectedMatch.getAwayTeam(), scoreboard.getMatches().get(0).getAwayTeam());
+    }
 
 }
