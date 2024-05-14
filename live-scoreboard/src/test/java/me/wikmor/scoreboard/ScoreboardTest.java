b48518d9bf929c58c9a67225922cd2339da4876a
wikmor <wikmor@proton.me>
2024-05-14 13:34:46 +0200

    Fix getSummary test

--- live-scoreboard/src/test/java/me/wikmor/scoreboard/ScoreboardTest.java
+++ live-scoreboard/src/test/java/me/wikmor/scoreboard/ScoreboardTest.java
@@ -3,6 +3,8 @@ package me.wikmor.scoreboard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreboardTest {
@@ -107,39 +109,50 @@ class ScoreboardTest {
         assertEquals("Brazil", scoreboard.getMatches().get(0).getAwayTeam().getName());
     }
 
-//    @Test
-//    void getSummary_shouldReturnOrderedScoreboard_whenCalled() throws InterruptedException {
-//        // Given
-//        scoreboard.addMatch("Mexico", "Canada");
-//        Thread.sleep(100);
-//        scoreboard.addMatch("Spain", "Brazil");
-//        Thread.sleep(100);
-//        scoreboard.addMatch("Germany", "France");
-//        Thread.sleep(100);
-//        scoreboard.addMatch("Uruguay", "Italy");
-//        Thread.sleep(100);
-//        scoreboard.addMatch("Argentina", "Australia");
-//
-//        scoreboard.updateScore("Mexico", "Canada", 0, 5);
-//        scoreboard.updateScore("Spain", "Brazil", 10, 2);
-//        scoreboard.updateScore("Germany", "France", 2, 2);
-//        scoreboard.updateScore("Uruguay", "Italy", 6, 6);
-//        scoreboard.updateScore("Argentina", "Australia", 3, 1);
-//
-//        // When
-//        List<Match> actualMatches = scoreboard.getSummary();
-//
-//        // Then
-//        assertEquals("Uruguay", actualMatches.get(0).getHomeTeam().getName());
-//        assertEquals(actualMatches.get(0).getAwayTeam().getName(), "Italy");
-//        assertEquals(actualMatches.get(1).getHomeTeam().getName(), "Spain");
-//        assertEquals(actualMatches.get(1).getAwayTeam().getName(), "Brazil");
-//        assertEquals(actualMatches.get(2).getHomeTeam().getName(), "Mexico");
-//        assertEquals(actualMatches.get(2).getAwayTeam().getName(), "Canada");
-//        assertEquals(actualMatches.get(3).getHomeTeam().getName(), "Argentina");
-//        assertEquals(actualMatches.get(3).getAwayTeam().getName(), "Australia");
-//        assertEquals(actualMatches.get(4).getHomeTeam().getName(), "Germany");
-//        assertEquals(actualMatches.get(4).getAwayTeam().getName(), "France");
-//    }
+    @Test
+    void getSummary_shouldReturnOrderedScoreboard_whenCalled() throws InterruptedException {
+        // Given
+        Team mexico = new Team("Mexico");
+        Team canada = new Team("Canada");
+        Team spain = new Team("Spain");
+        Team brazil = new Team("Brazil");
+        Team germany = new Team("Germany");
+        Team france = new Team("France");
+        Team uruguay = new Team("Uruguay");
+        Team italy = new Team("Italy");
+        Team argentina = new Team("Argentina");
+        Team australia = new Team("Australia");
+
+        scoreboard.addMatch(mexico, canada);
+        Thread.sleep(100);
+        scoreboard.addMatch(spain, brazil);
+        Thread.sleep(100);
+        scoreboard.addMatch(germany, france);
+        Thread.sleep(100);
+        scoreboard.addMatch(uruguay, italy);
+        Thread.sleep(100);
+        scoreboard.addMatch(argentina, australia);
+
+        scoreboard.updateScore(mexico, canada, 0, 5);
+        scoreboard.updateScore(spain, brazil, 10, 2);
+        scoreboard.updateScore(germany, france, 2, 2);
+        scoreboard.updateScore(uruguay, italy, 6, 6);
+        scoreboard.updateScore(argentina, australia, 3, 1);
+
+        // When
+        List<Match> actualMatches = scoreboard.getSummary();
+
+        // Then
+        assertEquals(uruguay, actualMatches.get(0).getHomeTeam());
+        assertEquals(italy, actualMatches.get(0).getAwayTeam());
+        assertEquals(spain, actualMatches.get(1).getHomeTeam());
+        assertEquals(brazil, actualMatches.get(1).getAwayTeam());
+        assertEquals(mexico, actualMatches.get(2).getHomeTeam());
+        assertEquals(canada, actualMatches.get(2).getAwayTeam());
+        assertEquals(argentina, actualMatches.get(3).getHomeTeam());
+        assertEquals(australia, actualMatches.get(3).getAwayTeam());
+        assertEquals(germany, actualMatches.get(4).getHomeTeam());
+        assertEquals(france, actualMatches.get(4).getAwayTeam());
+    }
 
 }
